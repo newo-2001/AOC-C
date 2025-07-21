84a7b4e48f731f45a7bec763f894df6ccc59bce2
newo-2001 <o.elderbroek@gmail.com>
2025-07-21 17:05:19 +0200

    Add hashmap test

--- test/collections/hashmaptests.c
+++ test/collections/hashmaptests.c
@@ -5,6 +5,17 @@
 void tearDown() {}
 void setUp() {}
 
+static HashMap new_str_map()
+{
+    HashMapOptions options = {
+        .bucket_count = 3,
+        .hash_function = hashmap_str_hash,
+        .key_comparer = hashmap_str_eq
+    };
+
+    return hashmap_new(sizeof(const char*), sizeof(const char*), options);
+}
+
 void test_new_hashmap_is_empty(void)
 {
     HashMap map = hashmap_new(0, 0, hashmap_default_options());
@@ -39,6 +50,22 @@ void test_hashmap_contains_valid_key(void)
     hashmap_destroy(map);
 }
 
+void test_hashmap_contains_valid_string_key(void)
+{
+    HashMap map = new_str_map();
+    const char *key = "test", *value = "string";
+
+    hashmap_insert(&map, &key, &value);
+    
+    const char* lookup = "test";
+    TEST_ASSERT_TRUE_MESSAGE(hashmap_contains_key(map, &lookup), "Hashmap didn't contain expected key");
+
+    const char* actual = *((const char**) hashmap_get(map, &lookup));
+    TEST_ASSERT_EQUAL_STRING(value, actual);
+
+    hashmap_destroy(map);
+}
+
 int main()
 {
     UNITY_BEGIN();
@@ -46,6 +73,7 @@ int main()
     RUN_TEST(test_new_hashmap_is_empty);
     RUN_TEST(test_hashmap_doesnt_contain_invalid_key);
     RUN_TEST(test_hashmap_contains_valid_key);
+    RUN_TEST(test_hashmap_contains_valid_string_key);
 
     return UNITY_END();
 }
